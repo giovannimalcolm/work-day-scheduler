44977bd71a6971bdfbc4584472376393480eb104
Giovanni Malcolm <giovannimalcolm@giovannis-mbp.lan>
2021-10-06 15:50:33 -0400

    Fixed up HTML. Added identifer class/ID  to textarea/timeblock for targeting. Removed p tag for timeblocks inside it's div to simply DOM manipulation

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,24 @@
+//need addEventListener to listen for save button clicks and save text content to local storage
 
-//do add event listener to save text content in box to local storage
-// need one for each button. cant target all buttons because it'll save everything on one click
+//div "hour-9" > timebox, textarea, and button (all siblings/kids)
+
+$('.saveBtn').on('click', saveData())
+
+//instead of doing global vars for every timeblock the event and time associated can be saved together
+function saveData() {
+    let event = $(this).siblings('.event').val(); //get text content of event 
+    let hour = $(this).parent().attr('id') //pull time that is associated with event from ID name
+
+    localStorage.setItem(hour, event)
+
+}
+
+
+function timeUpdate () {
+    let presentTime = moment().hours();
+
+
+    
+
+
+}
