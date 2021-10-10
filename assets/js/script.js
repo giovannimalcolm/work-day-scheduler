842d9e7e7dcb9b4b7952f53e46896b4e899fba96
Giovanni Malcolm <giovannimalcolm@Giovannis-MacBook-Pro.local>
2021-10-10 15:15:23 -0400

    Worked on during road trip. Functional base features with an added clear schedule button that removes all textcontent via local storage clearing and refreshes the page upon click.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,31 +1,39 @@
-//need addEventListener to listen for save button clicks and save text content to local storage
+let event;
+let hour;
 
-//div "hour-9" > timebox, textarea, and button (all siblings/kids)
+$(document).ready(function () {
+    $('#currentDay').text(moment().format('dddd, MMMM Do')); // display current day
+    timeUpdate();
 
-$('.saveBtn').on('click', saveData())
+    $('.saveBtn').on('click', function(){
+        let event = $(this).siblings('.event').val(); //get text content of event 
+        let hour = $(this).parent().attr('id') //pull time that is associated with event from ID name
+        localStorage.setItem(hour, event)
+        console.log(hour, event)
+    })
 
-//instead of doing global vars for every timeblock the event and time associated can be saved together
-function saveData() {
-    let event = $(this).siblings('.event').val(); //get text content of event 
-    let hour = $(this).parent().attr('id') //pull time that is associated with event from ID name
+    $('#block-9 .event').val(localStorage.getItem('block-9'));
+    $('#block-10 .event').val(localStorage.getItem('block-10'));
+    $('#block-11 .event').val(localStorage.getItem('block-11'));
+    $('#block-12 .event').val(localStorage.getItem('block-12'));
+    $('#block-13 .event').val(localStorage.getItem('block-13'));
+    $('#block-14 .event').val(localStorage.getItem('block-14'));
+    $('#block-15 .event').val(localStorage.getItem('block-15'));
+    $('#block-16 .event').val(localStorage.getItem('block-16'));
+    $('#block-17 .event').val(localStorage.getItem('block-17'));
 
-    localStorage.setItem(hour, event)
+})
 
-}
 
 
 function timeUpdate () {
     let presentTime = moment().hours();
-
     $('.hourBlock').each(function () {
         let selectedHour = parseInt($(this).attr('id').split('-')[1]);
-
-
         if (selectedHour < presentTime) {
-
             $(this).addClass('past');
         }
-        else if (blockHour === currentHour){
+        else if (selectedHour === presentTime){
             $(this).removeClass('past');
             $(this).addClass('present');
         }
@@ -34,25 +42,20 @@ function timeUpdate () {
             $(this).removeClass('present');
             $(this).addClass('future');
         }
-        
-
     });
-
     }
 
+    $('.clearBtn').on('click', function(){
+        localStorage.clear();
+        location.reload();
+
+    })
 
 
 
 
-function getData(){
-    for(i = 9; i < 18; i++){
-        $('#block-i .event').val(localStorage.getItem('block-i'));
-    }
 
-}
 
-$(document).ready(function () {
-    $('#currentDay').text(moment().format('dddd, MMMM Do')); // display current day
-    getData();
 
-})
+
+
